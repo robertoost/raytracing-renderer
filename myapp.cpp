c6b7a2fac217f230bc54145b24d711f698ac2ebd
robertoost <github@robertoo.st>
2021-11-29 04:32:29 +0100

    feat(init): add camera setup

--- myapp.cpp
+++ myapp.cpp
@@ -1,7 +1,9 @@
-#include "precomp.h"
+﻿#include "precomp.h"
 #include "myapp.h"
 
 TheApp* CreateApp() { return new MyApp(); }
+
+
 
 // -----------------------------------------------------------
 // Initialize the application
@@ -9,7 +11,20 @@ TheApp* CreateApp() { return new MyApp(); }
 void MyApp::Init()
 {
 	// anything that happens only once at application start goes here
-	string whaaaa = "whaaaa";
+
+	// Camera setup:
+
+	float d = 4.f;
+
+	float3 camPos = float3(0, 0, 0); 	// Camera position: 𝐸 =(0,0,0)
+	float3 viewDir = float3(0, 0, 1); 	// View direction : 𝑉 = (0, 0, 1)
+	float3 screenCenter = camPos + d * viewDir; 	// Screen center : 𝐶 = 𝐸 + 𝑑𝑉
+		// Screen corners : 𝑃0 = 𝐶 + −1, −1, 0, 𝑃1 = 𝐶 + 1, −1, 0, 𝑃2 = 𝐶 + (−1, 1, 0)
+
+	// Change FOV by altering 𝑑;
+	// Transform camera by multiplying E, 𝑃0, 𝑃1, 𝑃2 with a camera matrix.
+
+	// TODO: C++ how do I get this to the tick function?
 }
 
 // -----------------------------------------------------------
@@ -20,13 +35,13 @@ void MyApp::Tick( float deltaTime )
 	// clear the screen to black
 	screen->Clear( 0 );
 	// print something to the console window
-	//printf( "hello world!\n" );
-
+	printf( "hello world!\n" );
+	
 	// plot some colors
 	for( int red = 0; red < 256; red++ ) for( int green = 0; green < 256; green++ )
 	{
 		int x = red, y = green;
-		screen->Plot( x + 100, y + 100, (red << 16) + (green << 8) );
+		screen->Plot( x + 200, y + 100, (red << 16) + (green << 8) );
 	}
 	// plot a white pixel in the bottom right corner
 	screen->Plot( SCRWIDTH - 2, SCRHEIGHT - 2, 0xffffff );
