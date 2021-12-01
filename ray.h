3a60c4933c32ffe9d051abb1556cadfcf883c211
robertoost <github@robertoo.st>
2021-12-01 20:53:12 +0100

    feat(ray): add simple ray class

---
+++ ray.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <vec3.h>
+
+namespace RaytracingRenderer {
+
+	class Ray
+	{
+    public:
+        Ray() {}
+        Ray(const point3& origin, const vec3& direction)
+            : orig(origin), dir(direction)
+        {}
+
+        point3 origin() const { return orig; }
+        vec3 direction() const { return dir; }
+
+        point3 at(double t) const {
+            return orig + t * dir;
+        }
+
+    protected:
+        point3 orig;
+        vec3 dir;
+	};
+}
+
