9350510d1b8d6819ff87d940df7a5f37649ae9b0
robertoost <github@robertoo.st>
2021-12-03 19:32:37 +0100

    feat(color): use uint

--- color.h
+++ color.h
@@ -5,11 +5,10 @@
 namespace RaytracingRenderer {
 
     unsigned int translate_color(float3 pixel_color) {
-        uint8_t r = pixel_color.x * 255.f;
-        uint8_t g = pixel_color.y * 255.f;
-        uint8_t b = pixel_color.z * 255.f;
-
-        return (r << 16) + (g << 8) + b;
+        const uint ir = min((uint)(pixel_color.x * 255), 255u);
+        const uint ig = min((uint)(pixel_color.y * 255), 255u);
+        const uint ib = min((uint)(pixel_color.z * 255), 255u);
+        return (ir << 16) + (ig << 8) + ib;
     }
 }
 
