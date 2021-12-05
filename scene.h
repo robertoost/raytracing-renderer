f278c50fcc0939ac3b97f87cb2bef8202aa2f665
robertoost <github@robertoo.st>
2021-12-05 18:40:35 +0100

    feat(scene): generic list of scene objects

--- scene.h
+++ scene.h
@@ -5,15 +5,33 @@
 
 namespace RaytracingRenderer {
 
-	class Scene
+	class Scene: public Hittable
 	{
 	public:
 		Camera camera;
-		Sphere sphere;
-		Plane plane;
 
-		//list<Hittable> objects;
+		list<shared_ptr<Hittable>> objects;
+		bool intersect(const Ray& ray, float t_min, float t_max, hit_record& rec) const override {
+			bool collision = false;
+			
+			// Loop over all objects in the scene and try to find intersections.
+			for (shared_ptr<Hittable> obj : objects) {
+				const bool hit = obj->intersect(ray, t_min, t_max, rec);
+				if (hit) {
+					collision = true;
+					t_max = rec.t;
+				}
+			}
+			return collision;
+		};
 
-		Scene(): sphere(float3(0,0,4), 1.f, Material()), plane(float3(0,-1,0), float3(0,1,0), Material(float3(0,0,1))) {};
+		Scene() {
+			objects = list<shared_ptr<Hittable>>();
+			objects.push_back(make_shared<Sphere>(Sphere(float3(0, 0, 4), 1.f, Material())));
+			objects.push_back(make_shared<Plane>(Plane(float3(0, -1, 0), float3(0, 1, 0), Material(float3(0, 0, 1)))));
+		};
+		Scene(list<shared_ptr<Hittable>> objects) {
+			this->objects = objects;
+		}
 	};
 }
