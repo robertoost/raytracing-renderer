63c1aca23fcb0b6cbccce6ea4c7b2da6e792cbe2
robertoost <github@robertoo.st>
2021-12-06 08:21:43 +0100

    fix(color): import trouble

--- color.h
+++ color.h
@@ -1,14 +1,11 @@
 #pragma once
 
-#include <iostream>
-
 namespace RaytracingRenderer {
 
-    unsigned int translate_color(float3 pixel_color) {
-        const uint ir = min((uint)(pixel_color.x * 255), 255u);
-        const uint ig = min((uint)(pixel_color.y * 255), 255u);
-        const uint ib = min((uint)(pixel_color.z * 255), 255u);
+    inline uint translate_color(float3 pixel_color) {
+        const uchar ir = min((uint)(pixel_color.x * 255), 255u);
+        const uchar ig = min((uint)(pixel_color.y * 255), 255u);
+        const uchar ib = min((uint)(pixel_color.z * 255), 255u);
         return (ir << 16) + (ig << 8) + ib;
     }
 }
-
