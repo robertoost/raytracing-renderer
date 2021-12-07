b16da97be39feef40ca1afd1b843f5e0c6417a25
robertoost <github@robertoo.st>
2021-12-08 00:56:41 +0100

    feat(point_light): handle shadow rays

--- point_light.h
+++ point_light.h
@@ -9,13 +9,32 @@ namespace RaytracingRenderer {
 
         PointLight() : Light() {}
 
-        float3 illuminate(float3& position, float3& normal, float3& light_dir) {
-            float cos_a = clamp(dot(normal, light_dir), 0.f, 1.f);
+        float3 illuminate(float3& position, float3& normal, Scene& scene) {
+
+            // Get difference between light and position.
+            float3 diff = this->position - position;
+            float distance_to_light = length(diff);
+            float3 direction = normalize(diff);
+
+            // Prepare shadow raycast
+            Ray shadow_ray = Ray(position, direction);
+            hit_record rec = hit_record();
+
+            // Get collision.
+            bool collision = scene.intersect(shadow_ray, 0.0001, distance_to_light, rec);
+
+            // If the light is obscured, continue.
+            if (collision == true) {
+                return float3(0,0,0);
+            }
+
+
+            float cos_a = clamp(dot(normal, direction), 0.f, 1.f);
 
             if (cos_a == 0) {
                 return float3(0, 0, 0);
             }
-            return float3(1, 1, 1) * cos_a * intensity;
+            return float3(1, 1, 1) * cos_a * intensity / powf(distance_to_light, 2.f);
         }
     };
 }
