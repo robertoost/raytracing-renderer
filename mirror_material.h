48e1bfe0f19644de00bf460cf8d0b224e1a187cf
robertoost <github@robertoo.st>
2021-12-07 21:50:17 +0100

    feat(mirror): slightly darker color

--- mirror_material.h
+++ mirror_material.h
@@ -8,7 +8,7 @@ namespace RaytracingRenderer {
 		void color(
 			const Ray& r_in, const hit_record& rec, float3& attenuation
 		) const override {
-			attenuation = 0.5 * float3(rec.normal.x + 1, rec.normal.y + 1, rec.normal.z + 1);
+			attenuation = float3(0.99f, 0.99f, 0.99f);
 		}
 
 		MirrorMaterial() : Material(MIRROR) {}
