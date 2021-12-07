5860a5c2d1ab6354fca3d9cda642a982d9d89e35
robertoost <github@robertoo.st>
2021-12-07 21:49:59 +0100

    feat(myapp): new scene with walls

--- myapp.cpp
+++ myapp.cpp
@@ -1,4 +1,4 @@
-﻿#include "precomp.h"
+#include "precomp.h"
 #include "myapp.h"
 
 TheApp* CreateApp() { return new MyApp(); }
@@ -84,21 +84,33 @@ float3 MyApp::DirectIllumination(float3 &position, float3 &normal) {
 // -----------------------------------------------------------
 void MyApp::Init()
 {
-	// Instantiate material pointers.
-	shared_ptr<NormalMaterial> sphere1_mat = make_shared<NormalMaterial>(NormalMaterial());
-	shared_ptr<UnlitMaterial> sphere2_mat = make_shared<UnlitMaterial>(UnlitMaterial(float3(0.2, 0.1, 1)));
+	// Instantiate colored material pointers.
+	shared_ptr<DiffuseMaterial> purple_mat = make_shared<DiffuseMaterial>(DiffuseMaterial(float3(0.2, 0.1, 1)));
+	shared_ptr<DiffuseMaterial> pink_mat = make_shared<DiffuseMaterial>(DiffuseMaterial(float3(0.9f, 0.3f, 0.4f)));
+	shared_ptr<DiffuseMaterial> lavender_mat = make_shared<DiffuseMaterial>(DiffuseMaterial(float3(0.4f, 0.4f, 0.8f)));
+	shared_ptr<DiffuseMaterial> off_white_mat = make_shared<DiffuseMaterial>(DiffuseMaterial(float3(0.85f, 0.85f, 0.9f)));
+
+	// Special materials
+	shared_ptr<NormalMaterial> normal_mat = make_shared<NormalMaterial>(NormalMaterial());
 	shared_ptr<CheckerboardMaterial> plane_mat = make_shared<CheckerboardMaterial>(CheckerboardMaterial(float3(0.4f,0.2f,1.f), float3(0.4f, 1.f,0.7f)));
+	shared_ptr<MirrorMaterial> mirror_mat = make_shared<MirrorMaterial>(MirrorMaterial());
 
 	// Instantiate object pointers.
-	shared_ptr<Sphere> sphere1 = make_shared<Sphere>(Sphere(float3(2, 1, 4), 2, sphere1_mat));
-	shared_ptr<Sphere> sphere2 = make_shared<Sphere>(Sphere(float3(-2, 0, 4), 1, sphere2_mat));
-	shared_ptr<Plane> plane = make_shared<Plane>(Plane(float3(0, -1, 0), float3(0, 1, 0), plane_mat));
+	shared_ptr<Sphere> sphere1 = make_shared<Sphere>(Sphere(float3(2, 1, 4), 2, normal_mat));
+	shared_ptr<Sphere> sphere2 = make_shared<Sphere>(Sphere(float3(-2, 1, 4), 1, mirror_mat));
+	shared_ptr<Plane> floor = make_shared<Plane>(Plane(float3(0, -3, 0), float3(0, 1, 0), plane_mat));
+	shared_ptr<Plane> left_wall = make_shared<Plane>(Plane(float3(-6, 0, 0), float3(1, 0, 0), pink_mat));
+	shared_ptr<Plane> back_wall = make_shared<Plane>(Plane(float3(0, 0, 8), float3(0, 0, -1), off_white_mat));
+	shared_ptr<Plane> right_wall = make_shared<Plane>(Plane(float3(6, 0, 0), float3(-1, 0, 0), lavender_mat));
+	shared_ptr<Plane> ceiling = make_shared<Plane>(Plane(float3(0, 6, 0), float3(0, -1, 0), off_white_mat));
 
 	// Put all scene objects in a list
-	list<shared_ptr<Hittable>> objects = list<shared_ptr<Hittable>>({sphere1, sphere2, plane});
+	list<shared_ptr<Hittable>> objects = list<shared_ptr<Hittable>>({ left_wall, right_wall, back_wall, ceiling, sphere1, sphere2, floor});
 
-	shared_ptr<PointLight> point_light = make_shared<PointLight>(PointLight(float3(-2, 10, 0), 1));
-	list<shared_ptr<Light>> lights = list<shared_ptr<Light>>({ point_light });
+	shared_ptr<PointLight> point_light = make_shared<PointLight>(PointLight(float3(0, 5, 4), 0.66f));
+	shared_ptr<AmbientLight> ambient_light = make_shared<AmbientLight>(AmbientLight(0.34f));
+
+	list<shared_ptr<Light>> lights = list<shared_ptr<Light>>({ point_light, ambient_light });
 
 	scene = Scene(objects, lights);
 }
