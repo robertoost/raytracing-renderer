42058da8c4c36eb3279711d39e8b850d786a097c
Robert Oost <info@robertoo.st>
2021-12-13 20:11:13 +0100

    fix(directional_light): invert shadow ray dir

--- directional_light.h
+++ directional_light.h
@@ -6,25 +6,25 @@ namespace RaytracingRenderer {
     {
     public:
         DirectionalLight(const float3 direction, const float intensity) : Light(intensity) {
-            this->direction = direction;
+            this->direction = normalize(direction);
         }
 
         DirectionalLight() : DirectionalLight(float3(-0.2f,-0.7f,-0.1f), 1.f) {}
 
         float3 illuminate(float3& position, float3& normal, Scene& scene) {
             // Prepare shadow raycast
-            Ray shadow_ray = Ray(position, direction);
+            Ray shadow_ray = Ray(position, -direction);
             hit_record rec = hit_record();
 
             // Get collision.
-            bool collision = scene.intersect(shadow_ray, 0.0001f, FLT_MAX, rec);
+            bool collision = scene.intersect(shadow_ray, 0.0001, FLT_MAX, rec);
 
             // If the light is obscured, continue.
             if (collision == true) {
                 return float3(0, 0, 0);
             }
 
-            float cos_a = clamp(dot(normal, direction), 0.f, 1.f);
+            float cos_a = clamp(dot(normal, -direction), 0.f, 1.f);
 
             if (cos_a == 0) {
                 return float3(0, 0, 0);
