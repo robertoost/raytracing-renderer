fad0c4325b596d828cebd65ae0794e27466e442e
Rayne Blair <raynebblair@gmail.com>
2021-12-15 16:02:46 +0100

    fixed window size

--- template/common.h
+++ template/common.h
@@ -1,6 +1,6 @@
 // default screen resolution
-#define SCRWIDTH	300
-#define SCRHEIGHT	300
+#define SCRWIDTH	1920
+#define SCRHEIGHT	1080
 #define ASPECT_RATIO SCRWIDTH / SCRHEIGHT
 
 #define MAX_RECURSION_DEPTH 10
