3306625615d6930795076b5091c2d412d8197e43
Rayne Blair <raynebblair@gmail.com>
2021-12-28 16:59:53 +0100

    feat(triangleimp): implementing boundedplane via triangles

--- template/precomp.h
+++ template/precomp.h
@@ -1238,6 +1238,7 @@ public:
 #include "src/3d-objects/primitives/sphere.h"
 #include "src/3d-objects/primitives/plane.h"
 #include "src/3d-objects/primitives/triangle.h"
+#include "src/3d-objects/primitives/boundedplane.h"
 
 // Camera.
 #include "src/raytracing/camera.h"
