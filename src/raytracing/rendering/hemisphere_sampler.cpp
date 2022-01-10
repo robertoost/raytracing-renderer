18456aa9bb5836723438bbe6c497ed8ce4ab4706
robertoost <github@robertoo.st>
2022-01-10 02:12:20 +0100

    feat(hemisphere_sampling): use util

--- src/raytracing/rendering/hemisphere_sampler.cpp
+++ src/raytracing/rendering/hemisphere_sampler.cpp
@@ -28,41 +28,6 @@ namespace RaytracingRenderer {
 		}
 	}
 
-	inline void createCoordinateSystem(const float3& N, float3& Nt, float3& Nb)
-	{
-		if (std::fabs(N.x) > std::fabs(N.y))
-			Nt = float3(N.z, 0, -N.x) / sqrtf(N.x * N.x + N.z * N.z);
-		else
-			Nt = float3(0, -N.z, N.y) / sqrtf(N.y * N.y + N.z * N.z);
-		Nb = cross(N, Nt);
-	}
-	inline float3 uniformSampleHemisphere(const float& r1, const float& r2)
-	{
-		// cos(theta) = r1 = y
-		// cos^2(theta) + sin^2(theta) = 1 -> sin(theta) = srtf(1 - cos^2(theta))
-		float sinTheta = sqrtf(1 - r1 * r1);
-		float phi = 2 * PI * r2;
-		float x = sinTheta * cosf(phi);
-		float z = sinTheta * sinf(phi);
-		return float3(x, r1, z);
-	}
-	// TODO: reflect a random position on the hemisphere.
-	inline float3 diffuseReflection(float3 normal) {
-		float3 Nt, Nb;
-		createCoordinateSystem(normal, Nt, Nb);
-
-		float r1 = random_float();
-		float r2 = random_float();
-		
-		float3 random_dir = uniformSampleHemisphere(r1, r2);
-		float3 sampleWorld(
-			random_dir.x * Nb.x + random_dir.y * normal.x + random_dir.z * Nt.x,
-			random_dir.x * Nb.y + random_dir.y * normal.y + random_dir.z * Nt.y,
-			random_dir.x * Nb.z + random_dir.y * normal.z + random_dir.z * Nt.z);
-
-		return sampleWorld;
-	}
-
 	void HemisphereSampler::Sample(Ray &ray, float3& energy) {
 
 		hit_record rec = hit_record();
@@ -73,13 +38,16 @@ namespace RaytracingRenderer {
 			return;
 		}
 
+		if (rec.mat_ptr->emission() > 0) {
+			energy += (rec.mat_ptr->emission() * rec.mat_ptr->emission_color());
+			return;
+		}
+
 		float3 diffuseDir = diffuseReflection(rec.normal);
 
 		hit_record diffuse_rec = hit_record();
-		Ray diffuse_ray = Ray(rec.p + diffuseDir * 0.0001f, diffuseDir);
+		Ray diffuse_ray = Ray(rec.p, diffuseDir);
 		collision = scene->intersect(diffuse_ray, 0.001f, FLT_MAX, diffuse_rec);
-		
-		energy += (rec.mat_ptr->emission() * rec.mat_ptr->emission_color());
 
 		if (collision == false) {
 			return;
@@ -96,7 +64,4 @@ namespace RaytracingRenderer {
 			energy += 2.f * PI * BRDF * (diffuse_rec.mat_ptr->emission() * diffuse_rec.mat_ptr->emission_color()) * cos_i;
 		}
 	}
-
-
-
 }
--- src/raytracing/rendering/hemisphere_sampler.h
+++ src/raytracing/rendering/hemisphere_sampler.h
@@ -8,7 +8,7 @@ namespace RaytracingRenderer {
 		// When the camera updates, the accumulated energy is reset.
 		inline void OnCameraUpdate() override { accumulator.resetAccumulator(); };
 		void RenderScene(float3 frame[SCRHEIGHT][SCRWIDTH]) override;
-		void Sample(Ray& ray, float3& pixel_color);
+		void Sample(Ray& ray, float3& energy);
 	private:
 		Accumulator accumulator;
 	};
